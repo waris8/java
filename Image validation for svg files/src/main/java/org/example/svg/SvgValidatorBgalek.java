5897ee86e6542e94d8260aa3dcfb57ef6c029995
gulam waris <gulam.waris@timesinternet.in>
2022-02-28 23:57:02 +0530

    small changes

--- Image validation for svg files/src/main/java/org/example/svg/SvgValidatorBgalek.java
+++ Image validation for svg files/src/main/java/org/example/svg/SvgValidatorBgalek.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 
 public class SvgValidatorBgalek {
     public static void main(String[] args) throws Exception{
+        
         byte[] array = Files.readAllBytes(Paths.get("/Users/gulam.waris/sc1.png"));
         SvgSecurityValidator validator = new SvgSecurityValidator();
         System.out.println(validator.validate(array).hasViolations());
